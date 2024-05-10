c17dd543eea7172e2deb193cb21af02f70765173
ArunBalagani <arunkumarbalagani1997@gmail.com>
2024-05-10 14:23:18 +0530

    My First React Project

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>
           Arun Kumar Balagani Portfolio
+        </h1>
+        <p>
+          My First React Project
         </p>
+
        
       </header>
     </div>

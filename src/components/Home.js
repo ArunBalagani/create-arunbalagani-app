b3d1da1915e11544746201e3566022cb4465263e
Arun Kumar Balagani <165940401+ArunBalagani@users.noreply.github.com>
2024-10-24 15:19:17 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 // src/components/Home.js
 import React from 'react';
+import heroImage from '../images/pic-1.jpg';
 
 const Home = () => {
     return (
         <section id="home">
             <div className="hero">
-                <img src="profile.jpg" alt="Arun Kumar" className="profile-image" />
+                <img src={heroImage} alt="Arun Kumar" className="profile-image" />
                 <h1>Arun Kumar</h1>
                 <p>Full Stack Developer</p>
                 <a href="#projects" className="btn">View My Work</a>

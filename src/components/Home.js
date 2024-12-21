58cb8dddcdc63920c8d33baafcf7a8e303f7a53b
ArunBalagani <arunkumarbalagani1997@gmail.com>
2024-12-21 12:43:15 +0530

    portfolio updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,13 @@
 // src/components/Home.js
 import React from 'react';
-import heroImage from '../images/pic-1.jpg';
+// import heroImage from '../images/pic-1.jpg';
+import banner from '../images/banner.jpg'
 
 const Home = () => {
     return (
         <section id="home">
             <div className="hero">
-                <img src={heroImage} alt="Arun Kumar" className="profile-image" width="300" height="200" />
+                <img src={banner} alt="Arun Kumar" className="profile-image" width="300" height="200" />
                 <h1>Arun Kumar</h1>
                 <p>Full Stack Developer</p>
                 <a href="#projects" className="btn">View My Work</a>
